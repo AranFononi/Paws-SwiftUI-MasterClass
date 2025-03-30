3626c3ae8e5173ba0fa4b62a47baf28b99f98d0e
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 01:05:46 +0330

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Aran Fononi on 31/3/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
