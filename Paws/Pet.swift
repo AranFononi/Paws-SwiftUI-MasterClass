d706bab05ef0cde8f7b986d47d7f154f56e8dc9a
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 01:12:01 +0330

    Create Pet Data Model

---
+++ Paws/Pet.swift
@@ -0,0 +1,20 @@
+//
+//  Pet.swift
+//  Paws
+//
+//  Created by Aran Fononi on 31/3/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Pet {
+    var name: String
+    @Attribute(.externalStorage) var photo: Data?
+    
+    init(name: String, photo: Data? = nil) {
+        self.name = name
+        self.photo = photo
+    }
+}
