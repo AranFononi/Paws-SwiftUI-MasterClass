6a952572b58c466308de8e13c8fa53b387fbeaaf
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 02:36:07 +0330

    Add Trash Button and Edit Button

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
     @Query private var pets: [Pet]
     
     @State private var path = [Pet]()
+    @State private var isEditing: Bool = false
     
     let layout = [
         GridItem(.flexible(minimum: 120)),
@@ -20,6 +21,7 @@ struct ContentView: View {
     ]
     
     func addPet() {
+        isEditing = false
         let pet = Pet(name: "Best Friend")
         modelContext.insert(pet)
         path = [pet]
@@ -40,7 +42,7 @@ struct ContentView: View {
                                                 .resizable()
                                                 .scaledToFit()
                                                 .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
-                                    
+                                            
                                         }
                                     } else {
                                         Image(systemName: "pawprint.circle")
@@ -58,6 +60,27 @@ struct ContentView: View {
                                 .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity)
                                 .background(.ultraThinMaterial)
                                 .clipShape(RoundedRectangle(cornerRadius: 8, style: .circular))
+                                .overlay(alignment: .topTrailing) {
+                                    if !isEditing {
+                                        Menu {
+                                            Button("Delete", systemImage: "trash", role: .destructive) {
+                                                withAnimation {
+                                                    modelContext.delete(pet)
+                                                    try? modelContext.save()
+                                                }
+                                                
+                                            }
+                                        } label: {
+                                            Image(systemName: "trash.circle.fill")
+                                                .resizable()
+                                                .aspectRatio(contentMode: .fill)
+                                                .frame(width: 36, height: 36)
+                                                .foregroundStyle(.red)
+                                                .symbolRenderingMode(.multicolor)
+                                                .padding()
+                                        }
+                                    }
+                                }
                             } // Navlink
                             .foregroundStyle(.primary)
                         } // ForEach
@@ -68,6 +91,17 @@ struct ContentView: View {
             .navigationTitle(pets.isEmpty ? "" : "Paws")
             .navigationDestination(for: Pet.self, destination: EditPetView.init)
             .toolbar {
+                
+                ToolbarItem(placement: .topBarLeading) {
+                    Button{
+                        withAnimation {
+                            isEditing.toggle()
+                        } label: {
+                            Image(systemName: "slider.horizontal.3")
+                        }
+                    }
+                }
+                
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add a New Pet", systemImage: "plus.circle", action: addPet)
                 }
