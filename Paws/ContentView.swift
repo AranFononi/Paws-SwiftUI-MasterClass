71e4543f6ac602d11273b4492c9b10fdcfc8c5b5
Aran Fononi <aaronfononi@gmail.com>
2025-03-31 01:20:42 +0330

    Add Custom Content Unavailable View To ContentView

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -6,19 +6,27 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query private var pets: [Pet]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ScrollView {
+                EmptyView()
+            } // ScrollView
+            .overlay {
+                if pets.isEmpty {
+                    CustomContentUnavailableView(icon: "cat.circle", title: "No Pets", description: "Add a new pet to get started.")
+                }
+            }
+        } // Navigation Stack
     }
 }
 
-#Preview {
+#Preview("No Data") {
     ContentView()
+        .modelContainer(for: Pet.self, inMemory: true)
 }
